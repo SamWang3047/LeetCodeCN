3169c8641a32c674e1f125cc988e8a3ba9e5ed65
SamWang3047 <w304714766@gmail.com>
2022-10-17 17:11:43 +0800

    20221017

---
+++ editor/cn/[904]水果成篮.java
@@ -0,0 +1,24 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int n = fruits.length;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+
+        int left = 0, ans = 0;
+        for (int right = 0; right < n; ++right) {
+            cnt.put(fruits[right], cnt.getOrDefault(fruits[right], 0) + 1);
+            while (cnt.size() > 2) {
+                cnt.put(fruits[left], cnt.get(fruits[left]) - 1);
+                if (cnt.get(fruits[left]) == 0) {
+                    cnt.remove(fruits[left]);
+                }
+                ++left;
+            }
+            ans = Math.max(ans, right - left + 1);
+        }
+        return ans;
+    }
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
